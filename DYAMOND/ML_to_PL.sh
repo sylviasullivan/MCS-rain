35d970acea5b39a07710e6d9687c8b306d30f9df
Sylvia Sullivan <sylvia@arizona.edu>
2024-03-05 15:13:03 -0700

    Separating figures out to individual ipynbs

---
+++ DYAMOND/ML_to_PL.sh
@@ -0,0 +1,35 @@
+#Build a command to remap the model level output to the pressure levels
+# given in PMEAN_*-*.txt
+
+# Directory and inputs to build the file names.
+basedir='/xdisk/sylvia/DYAMOND-UM/'
+fileprefix='ta'
+filesuffix='_3hr_HadGEM3-GA71_N2560_20160801-20160908.nc_tropics.nc'
+inputfile=$basedir$fileprefix$filesuffix
+echo $file
+
+# Read the pressure levels from the txt file. Add them line by line to $pressures.
+# c counts how many pressures are specified.
+c=0
+pressures=
+pfull_mean='/groups/sylvia/JAS-MCS-rain/DYAMOND/mean_pfull_3hr_HadGEM3-GA71_N2560_20160801-20160908.txt'
+phalf_mean='/groups/sylvia/JAS-MCS-rain/DYAMOND/mean_phalf_3hr_HadGEM3-GA71_N2560_20160801-20160908.txt'
+while read -r line; do pressures=$pressures$line','; c=$((c+1)); done < $pfull_mean
+echo $c' pressures specified'
+
+# Remove the last comma from $pressures.
+pressures="${pressures%?}"
+echo $pressures
+
+# Assemble the filenames and command.
+part1='cdo ml2pl,'
+outputfile=$basedir$fileprefix'_3hr_HadGEM3-GA71_N2560_20160801-20160908_PLfull.nc'
+#outputfile=$basedir$fileprefix'_3hr_HadGEM3-GA71_N2560_20160801-20160908_PLhalf.nc'
+
+# Rename pres as air_pressure in the $inputfile.
+#cdo chname,pres,air_pressure $inputfile $basedir'temp.nc'
+cmd=$part1$pressures' '$inputfile' '$outputfile
+echo ${cmd}
+
+# Evaluate the command as you would from the command line.
+eval $cmd
